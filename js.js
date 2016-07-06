a5ff3c58ea67d531aab8ab34110961e9ab42665f
Jonathan Voxland <Jonathan Voxland>
2016-07-06 12:02:02 -0700

    Randomized cards but both sides will randomize in same spot. Need to correct and make it random for both left and right side

--- js.js
+++ js.js
@@ -19,39 +19,35 @@ var Game = function(){
     this.cards = [];
 // This refers to an instance of Game Class
 };
-// var Shuffle = function(array){
-//     var currentIndex = array.length, temporaryValue, randomIndex;
-//
-//     // While there remain elements to shuffle...
-//     while (0 !== currentIndex) {
-//
-//         // Pick a remaining element...
-//         randomIndex = Math.floor(Math.random() * currentIndex);
-//         currentIndex -= 1;
-//
-//         // And swap it with the current element.
-//         temporaryValue = array[currentIndex];
-//         array[currentIndex] = array[randomIndex];
-//         array[randomIndex] = temporaryValue;
-//     }
-//
-//     return array;
-// };
-// Shuffle(game.cards);
 //Defining a Method (setUpGame) on the Game Class
 Game.prototype.setUpGame = function(){
   //This is an instance of game calling createCards and renderCards on THIS instance of Game
-    this.createCards(9);
+    this.createCardsAndRandomize(9);
     this.renderCards();
+};
+Game.prototype.createCardsAndRandomize = function(cardCount) { //Review with Chris or Dan!!!!!!!!
+    var leftList = new Array(9);
+    var rightList = new Array(9);
+    for (i = 1; i <=cardCount; i++){
+        leftList[i]= Math.floor(Math.random() * 9) + 1;
+        rightList[i]= Math.floor(Math.random() * 9) + 1;
+        this.cards.push(
+            // new Card('Images/' + i + '.jpg',i)
+            new Card(leftList[i],rightList[i])
+
+        )
+    }
 };
 //Defining a Method (renderCards) on the Game Class
 Game.prototype.renderCards = function() {
-    var game = this;
-    this.cards.forEach(function(card) {
-        var cardHtmlString =  '<div class="card"><div class = "front card-' + card.id + '"></div><div class = "back"></div></div>';
+    this.cards.forEach(function(cards) {
+        var leftCardHtmlString =  '<div class="card"><div class = "front card-' + cards.id + '"></div><div class =' +
+            ' "back"></div></div>';
+        var rightCardHtmlString =  '<div class="card"><div class = "front card-' + cards.id + '"></div><div class =' +
+            ' "back"></div></div>';
         // var cardHtmlString = "<img data-id='" + card.id + "' src='" + card.backImage + "' class='card card-" + card.id + "'>";
-        var leftCardElement = $(cardHtmlString);
-        var rightCardElement = $(cardHtmlString);
+        var leftCardElement = $(leftCardHtmlString);
+        var rightCardElement = $(rightCardHtmlString);
         $(".right_side").append(rightCardElement);
         $(".left_side").append(leftCardElement);
         leftCardElement.on('click', function(event){{
@@ -64,44 +60,8 @@ Game.prototype.renderCards = function() {
         });
     })
 };
-Game.prototype.createCards = function(cardCount) {
-    for (i = 1; i <=cardCount; i++){
-        Math.floor(Math.random() * 5 );
-        this.cards.push(
-            // new Card('Images/' + i + '.jpg',i)
-        new Card(i)
-        )
-    }
-};
+
 $(document).ready(function() {
     game = new Game();
     game.setUpGame();
 });
-//game1 = new Game();
-//game1.fart = function() {console.log('fart')};
-//game1.fart();
-// will out put 'fart'
-//game2 = new Game();
-//game2.fart();
-//will blow up because fart is defined on the instance of a game class
-//Defining getCards methods on the game class every instance of Game will have this function
-
-/*
-cardCreation();
-function addImages(){
-    $(function cardImageLeft() {
-        for (var x = 0; x < 2; x++) {
-           console.log('firstloop');
-            for (var j = 0; j < 1; j++) {
-                $(".card.left").addClass("card1");
-                console.log('secondloop');
-            }
-        }});
-    $(function cardImageRight() {
-        for (var x = 0; x < 9; x++) {
-            $(".card.right").addClass("card2");
-        }
-    });
-}
-addImages();
-*/
