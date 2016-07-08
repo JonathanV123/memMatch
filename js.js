423df1fe848e5d7878a02ffe4c9593f502ee5e0a
Jonathan Voxland <Jonathan Voxland>
2016-07-08 15:38:20 -0700

    got 2 sets of cards for each ID just need to split to left and right side

--- js.js
+++ js.js
@@ -11,52 +11,60 @@
 // match_counter = 0;
 //Creating Card CLASS that takes 1 parameter
 var Card = function(id) {
-    //THIS refers to instance of Card Class
+    //THIS refers to instance of Card ClassGa
     this.id = id;
 };
 //Class Game
 var Game = function(){
     this.cards = [];
+    console.log(this);
 // This refers to an instance of Game Class
 };
 //Defining a Method (setUpGame) on the Game Class
 Game.prototype.setUpGame = function(){
   //This is an instance of game calling createCards and renderCards on THIS instance of Game
-  //   this.createCards(9);
-    this.renderCards();
-    this.randomizeCards(9);
+  this.createCards(9);
+  this.renderCards();
+ // this.randomizeCards(9);
 };
-// Game.prototype.createCards = function(cardCount) { //Review
-//     for (i = 1; i <= cardCount; i++) {
-//         // listId[i] = Math.floor(Math.random() * 9) + 1;
-//         this.cards.push(
-//             // new Card('Images/' + i + '.jpg',i)
-//             new Card(i)
-//         )
-//     }
-//     // for (var i = game.cards.length - 1; i > 0; i--) {
-//     //     var j = Math.floor(Math.random() * (i + 1));
-//     //     var temp = this.cards[i];
-//     //     this.cards[i] = this.cards[j];
-//     //     this.cards[j] = temp;
-//     // }
-// };
-Game.prototype.randomizeCards = function(count){
-    for (var i = count - 1; i > 0; i--){
-        console.log("ID" + i);
+Game.prototype.createCards = function(cardCount) { //Review
+    for (i = 1; i <= cardCount; i++) {
+        // listId[i] = Math.floor(Math.random() * 9) + 1;
+        this.cards.push(
+            // new Card('Images/' + i + '.jpg',i)
+            new Card(i +' createCardLoop')
+        )
+    }
+    for (j = 1; j <= cardCount; j++) {
+        // listId[i] = Math.floor(Math.random() * 9) + 1;
+        this.cards.push(
+            // new Card('Images/' + i + '.jpg',i)
+            new Card(j +' createCardLoop')
+        )
+    }
+    for (var i = game.cards.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = this.cards[i];
         this.cards[i] = this.cards[j];
         this.cards[j] = temp;
-        this.cards.push(
-            // new Card('Images/' + i + '.jpg',i)
-            new Card(i))
     }
 };
+// Game.prototype.randomizeCards = function(count){
+//     for (var i = count - 1; i > 0; i--){
+//         console.log("ID" + i);
+//         var j = Math.floor(Math.random() * (i + 1));
+//         var temp = this.cards[i];
+//         this.cards[i] = this.cards[j];
+//         this.cards[j] = temp;
+//         this.cards.push(
+//             // new Card('Images/' + i + '.jpg',i)
+//             new Card(i +' fail shuffle'))
+//     }
+// };
 //Defining a Method (renderCards) on the Game Class
 Game.prototype.renderCards = function() {
     this.cards.forEach(function() {
-        var CardHtmlString =  '<div class="card"><div class = "front card-' + + '"></div><div class' +
+        var CardHtmlString =  '<div class="card"><div class = "front card-' +  + '"></div><div class' +
             ' =' + ' "back"></div></div>';
         // var cardHtmlString = "<img data-id='" + card.id + "' src='" + card.backImage + "' class='card card-" + card.id + "'>";
         var leftCardElement = $(CardHtmlString);
