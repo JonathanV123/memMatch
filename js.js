4afdcdd528b8460d9858569eb8b49629aec66ce8
Jonathan Voxland <Jonathan Voxland>
2016-07-28 15:26:33 -0700

    Completed card match to the class so it will add the class correctly with Jquery. Need to work on making it so that when a 3rd card is clicked you can't catch a quick glimpse of it

--- js.js
+++ js.js
@@ -13,6 +13,7 @@
 var Game = function(){
     this.leftCards = [];
     this.rightCards = [];
+    this.checkCards = 0;
 // This refers to an instance of Game Class
 };
 //Creating Card CLASS that takes 1 parameter
@@ -22,7 +23,7 @@ var Card = function(id) {
 };
 //Defining a Method (setUpGame) on the Game Class
 Game.prototype.setUpGame = function(){
-  //This is an instance of game calling createCards and renderCards on THIS instance of Game
+    //This is an instance of game calling createCards and renderCards on THIS instance of Game
   this.createCards(9);
   this.shuffleCards(this.leftCards);
   this.shuffleCards(this.rightCards);
@@ -49,49 +50,46 @@ Game.prototype.shuffleCards = function(cards){
     }
 };
 //Defining a Method (renderCards) on the Game Class
-Game.prototype.renderCards = function(){
-    for(var i = 0; i < game.leftCards.length; i++){
-            var CardHtmlStringLeft =  '<div class="card"> <div class = "front card-' +  game.leftCards[i].id  + '"></div><div' + ' class' +
+Game.prototype.renderCards = function() {
+    for (var i = 0; i < game.leftCards.length; i++) {
+        var CardHtmlStringLeft = '<div class="card"> <div class = "front card-' + game.leftCards[i].id + '"></div><div' + ' class' +
             ' =' + ' "back"></div></div>';
-            var CardHtmlStringRight =  '<div class="card"> <div class = "front card-' + game.rightCards[i].id  + '"></div><div' +
+        var CardHtmlStringRight = '<div class="card"> <div class = "front card-' + game.rightCards[i].id + '"></div><div' +
             ' class' + ' =' + ' "back"></div></div>';
-            var leftCardElement = $(CardHtmlStringLeft);
-            var rightCardElement = $(CardHtmlStringRight);
-            $(".right_side").append(rightCardElement);
-            $(".left_side").append(leftCardElement);
-            leftCardElement.on('click',function(event){{
-                $(this).toggleClass('flipcard');
-                console.log(this);
-            }
-            });
-            rightCardElement.on('click',function(event){{
-                $(this).toggleClass('flipcard');
-            }
+        var leftCardElement = $(CardHtmlStringLeft);
+        var rightCardElement = $(CardHtmlStringRight);
+        $(".right_side").append(rightCardElement);
+        $(".left_side").append(leftCardElement);
+        if (game.checkCards < 2) {
+            leftCardElement.on('click', function (event) {
+                {
+                    $(this).toggleClass('flipcard');
+                    game.checkCards++;
+                    console.log(game.checkCards);
+                }
+            })
+        }
+       else if (game.checkCards >= 2) {
+            game.checkCards = 0;
+            leftCardElement.on('click', function (event) {
+                $(this).toggleClass('emptyClass');
             });
         }
-            }
+    }
+}
 };
-// Game.prototype.updateGame = setTimeout(function(){
-//     // console.log(this === window );
-//     // console.log('di');
-//     // if($('cards').hasClass('flipcard')){
-//     //     $('CardHtmlStringRight').removeClass('flipcard')
-//     // }
-//     // if(this === window) {
-//     //     console.log('itd isd')
-//     // }
-//     // },2000);
 Game.prototype.cardMatch = function() {
-    setTimeout(this.cardMatch.bind(this),2000);
-        console.log(this === window);
-        console.log('doopedr');
-        if ($('card').hasClass('flipcard')) {
-            console.log('fart');
-            $('card').removeClass('flipcard')
-        }
-        if (this === window) {
-            console.log('it isd')
-        }
+    setTimeout(this.cardMatch.bind(this),1700);
+    console.log('fart');
+    // if (game.checkCards = 0 ){
+    //    game.checkCards ++;
+    //    console.log(game.checkCards)
+    // }
+    if (game.checkCards >= 2) {
+        game.checkCards = 0;
+        console.log('Check Complete');
+        $('.card').removeClass('flipcard');
+    }
 };
 $(document).ready(function() {
     game = new Game();
